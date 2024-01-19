10fcfc4b90f132f26a2c22ad59cb6a40b65344e4
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-19 17:23:27 +0100

    2.15 Usage of wrong parameter of type

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,4 @@ console.log(`${collection.userName}'s Todo List`);
 let newId = collection.addTodo("Go for run");
 let todoItem = collection.getTodoById(newId);
 todoItem.printDetails();
+// collection.addTodo(todoItem);   // Argument of type 'TodoItem' is not assignable to parameter of type 'string'.ts(2345)
