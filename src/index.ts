79e1ee81b1f10a86a33fd4860c0ecdf49c9fdd5b
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-19 17:39:25 +0100

    2.16 Adding type information to index file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
 
-let todos = [
+let todos : TodoItem[] = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 
-let collection = new TodoCollection("Maciek", todos);
+let collection : TodoCollection = new TodoCollection("Maciek", todos);
 
 console.clear();
 console.log(`${collection.userName}'s Todo List`);
 
-let newId = collection.addTodo("Go for run");
-let todoItem = collection.getTodoById(newId);
+let newId : number = collection.addTodo("Go for run");
+let todoItem : TodoItem = collection.getTodoById(newId);
 todoItem.printDetails();
 // collection.addTodo(todoItem);   // Argument of type 'TodoItem' is not assignable to parameter of type 'string'.ts(2345)
