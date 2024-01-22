70297cbe91c23594e894eafe07d3f6f66d969100
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-22 10:50:00 +0100

    2.25-2.27 Providing item counts

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,12 @@ let todos : TodoItem[] = [
 let collection : TodoCollection = new TodoCollection("Maciek", todos);
 
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
+// console.log(`${collection.userName}'s Todo List`);
+console.log(`${collection.userName}' Todo List ` + ` (${collection.getItemCounts().incomplete } items to do)`);
 
 // let newId : number = collection.addTodo("Go for run");
 // let todoItem : TodoItem = collection.getTodoById(newId);
 // todoItem.printDetails();
 // collection.addTodo(todoItem);   // Argument of type 'TodoItem' is not assignable to parameter of type 'string'.ts(2345)
-collection.removeComplete();
+// collection.removeComplete();
 collection.getTodoItems(true).forEach(item => item.printDetails());
