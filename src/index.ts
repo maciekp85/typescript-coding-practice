e7799d15c79b57b8827ddf566dce47d47a61425b
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-22 15:06:07 +0100

    2.42 Marking tasks complete and removing completed tasks. Adding new commands

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,10 @@ function displayTodoList(): void {
 
 enum Commands {
     Add = "Add New Task",
+    Complete = "Complete Task",
+    Toogle = "Show/Hide Completed",
+    Purge = "Remove Completed Tasks",
     Quit = "Quit",
-    Toogle = "Show/Hide Completed"
 }
 
 function promptAdd(): void {
@@ -31,6 +33,24 @@ function promptAdd(): void {
         if (answers["add"] !== "") {
             collection.addTodo(answers["add"]);
         }
+        promptUser();
+    })
+}
+
+function promptComplete(): void {
+    console.clear();
+    inquirer.prompt({
+        type: "checkbox",
+        name: "complete",
+        choices: collection.getTodoItems(showCompleted).map(item => ({
+            name: item.task,
+            value: item.id,
+            checked: item.complete
+        }))
+    }).then(answers => {
+        let completedTasks = answers["complete"] as number[];
+        collection.getTodoItems(true).forEach(item => 
+            collection.markComplete(item.id, completedTasks.find(id => id === item.id) != undefined));
         promptUser();
     })
 }
@@ -52,6 +72,17 @@ function promptUser(): void {
                 break;
             case Commands.Add:
                 promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                } else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
                 break;
         }
     })
