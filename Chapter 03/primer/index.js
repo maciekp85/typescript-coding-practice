7b2fca9b3b52ff10234b142d45058e780b8d2066
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-23 13:50:27 +0100

    3.10 Avoiding unintentional type coercion

--- Chapter 03/primer/index.js
+++ Chapter 03/primer/index.js
@@ -3,13 +3,13 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-if (hatPrice == bootsPrice) {
+if (hatPrice === bootsPrice) {
     console.log("Prices are the same");
 } else {
     console.log("Prices are different");
 }
 
-let totalPrice = hatPrice + bootsPrice;
+let totalPrice = Number(hatPrice) + Number(bootsPrice);
 console.log(`Total Price: ${totalPrice}`);
 
 let myVariable = "Adam";
