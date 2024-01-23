ceac7430b6b1f4869a54ace25c418e551d4cb574
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-23 13:29:00 +0100

    3.6-3.9 Working with primitive data types

--- Chapter 03/primer/index.js
+++ Chapter 03/primer/index.js
@@ -2,3 +2,25 @@ let hatPrice = 100;
 console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
+
+if (hatPrice == bootsPrice) {
+    console.log("Prices are the same");
+} else {
+    console.log("Prices are different");
+}
+
+let totalPrice = hatPrice + bootsPrice;
+console.log(`Total Price: ${totalPrice}`);
+
+let myVariable = "Adam";
+console.log(`Type: ${typeof myVariable}`);
+
+myVariable = 100;
+console.log(`Type: ${typeof myVariable}`);
+
+/*
+When the typeof keyword is used on null values, the result is object.
+This is a long-standing behavior that dates back to the earliest days of JavaScript and that hasn’t
+been changed because so much code has been written that expects this behavior.
+*/
+console.log(`Type of null: ${typeof null}`);
