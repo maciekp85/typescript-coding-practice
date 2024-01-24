f788b50d943490aad77daa5c0b0431740785c5c0
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-24 15:11:52 +0100

    3.21 Working with arrays. Defining and using an array

--- Chapter 03/primer/index.js
+++ Chapter 03/primer/index.js
@@ -3,14 +3,17 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
+let names = ["Hat", "Boots", "Gloves"];
+let prices = [];
+
+prices.push(100);
+prices.push("100");
+prices.push(50.25);
+
+console.log(`First Item: ${names[0]}: ${prices[0]}`);
+
 let sumPricess = (...numbers) => numbers.reduce((total, value) => 
     total + (Number.isNaN(Number(value)) ? 0 : Number(value)));
 
-let totalPrice = sumPricess(hatPrice, bootsPrice);
-console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
-
-totalPrice = sumPricess(100, 200, 300);
-console.log(`Total price: ${totalPrice} ${typeof totalPrice}`);
-
-totalPrice = sumPricess(100, 200, undefined, false, "hello");
-console.log(`Total price: ${totalPrice} ${typeof totalPrice}`);
+let totalPrice = sumPricess(...prices);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
