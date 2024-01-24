54f59ed104a6654b5106b74e76d3309a9878180f
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-24 14:09:54 +0100

    3.19 Using an arrow function

--- Chapter 03/primer/index.js
+++ Chapter 03/primer/index.js
@@ -4,9 +4,8 @@ let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
 function sumPricess(...numbers) {
-    return numbers.reduce(function(total, val) {
-        return total + (Number.isNaN(Number(val)) ? 0 : Number(val));
-    }, 0);
+    return numbers.reduce((total, val) => 
+        total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
 }
 
 let totalPrice = sumPricess(hatPrice, bootsPrice);
