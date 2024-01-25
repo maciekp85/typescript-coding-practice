ee317cd4cac03a438b369027549982a7933ebce5
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-25 16:59:22 +0100

    3.35 Using the fat arrow syntax with this keyword

--- Chapter 03/primer/index.js
+++ Chapter 03/primer/index.js
@@ -12,9 +12,8 @@ let hat = {
         return this._price;
     },
 
-    writeDetails() {
-        console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`);
-    }
+    writeDetails: () => 
+        console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`)
 };
 
 let boots = {
