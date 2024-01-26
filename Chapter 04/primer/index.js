bed6a2ebfbb4c5183b4e2a6405c37d41d2c145bb
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-26 12:16:48 +0100

    4.4 Comparing prototypes

--- Chapter 04/primer/index.js
+++ Chapter 04/primer/index.js
@@ -6,5 +6,21 @@ let hat = {
     }
 };
 
+let boots = {
+    name: "Boots",
+    price: 100,
+    getPriceIncTax() {
+        return Number(this.price) * 1.2;
+    }
+}
+
+let hatPrototype = Object.getPrototypeOf(hat);
+console.log(`Hat Prototype: ${hatPrototype}`);
+
+let bootsPrototype = Object.getPrototypeOf(boots);
+console.log(`Boots Prototype: ${bootsPrototype}`);
+
+console.log(`Common Prototype: ${ hatPrototype === bootsPrototype }`);
+
 console.log(`Hat: ${hat.price}, ${hat.getPriceIncTax() }`);
 console.log(`toString(): ${hat.toString()}`);
