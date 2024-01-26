b9f86b1369802236d7fabfc5ff9e687f72914a3e
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-26 12:12:09 +0100

    4.3 Understanding JavaScript object inheritance

--- Chapter 04/primer/index.js
+++ Chapter 04/primer/index.js
@@ -1,9 +1,10 @@
 let hat = {
     name: "Hat",
-    price: 120,
+    price: 100,
     getPriceIncTax() {
         return Number(this.price) * 1.2;
     }
 };
 
 console.log(`Hat: ${hat.price}, ${hat.getPriceIncTax() }`);
+console.log(`toString(): ${hat.toString()}`);
