fe916bec9d68686aa273a6b816934d56f095cbfd
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-27 15:09:23 +0100

    4.21 Using JavaScript collections. Storing data by key using an object

--- Chapter 04/primer/index.js
+++ Chapter 04/primer/index.js
@@ -9,29 +9,10 @@ class Product {
     }
 }
 
-
-class GiftPack {
-    constructor(name, prod1, prod2, prod3) {
-        this.name = name;
-        this.prod1 = prod1;
-        this.prod2 = prod2;
-        this.prod3 = prod3;
-    }
-
-    getTotalPrice() {
-        return [this.prod1, this.prod2, this.prod3].reduce((total, p) => total + p.price, 0);
-    }
-
-    *[Symbol.iterator]() {
-        yield this.prod1;
-        yield this.prod2;
-        yield this.prod3;
-    }
+let data = {
+    hat: new Product("Hat", 100)
 }
 
+data.boots = new Product("Boots", 100);
 
-let winter = new GiftPack("winter", new Product("Hat", 100), new Product("Boots", 80), new Product("Gloves", 23));
-
-console.log(`Total price: ${winter.getTotalPrice()}`);
-
-[...winter].forEach(p => console.log(`Product: ${p}`));
+Object.keys(data).forEach(key => console.log(data[key].toString()));
