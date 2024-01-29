f28f5930f4a6458fc595de13b60f3cc8ad0b91b4
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-29 14:13:42 +0100

    5.23-5.24 Selecting module format. Old CommonJs module format

--- Chapter 05/tools/src/index.ts
+++ Chapter 05/tools/src/index.ts
@@ -1,9 +1,9 @@
+import { sum } from "./calc";
+
 let printMessage = (msg: string): void => console.log(`Message: ${ msg }`);
 
 let message = ("Hello, TypeScript");
 printMessage(message);
 
-let data = new Map();
-data.set("Bob", "London");
-data.set("Alice", "Paris");
-data.forEach((val, key) => console.log(`${key} lives in ${val}`));
+let total = sum(100, 200, 300);
+printMessage(`Total: ${total}`);
