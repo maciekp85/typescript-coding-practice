04ae8afddda98b50755a0bd2b377f3b47ee667e6
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-29 09:16:49 +0100

    4.24 Storing data by index. Using a set

--- Chapter 04/primer/index.js
+++ Chapter 04/primer/index.js
@@ -4,28 +4,17 @@ class Product {
         this.name = name;
         this.price = price;
     }
-
-    toString() {
-        return `toString: Name: ${this.name}, Price: ${this.price}`;
-    }
 }
 
-class Supplier {
-    constructor(name, productids) {
-        this.name = name;
-        this.productids = productids;
-    }
+let product = new Product("Hat", 100);
+
+let productArray = [];
+let productSet = new Set();
+
+for (let i = 0; i < 5; i++) {
+    productArray.push(product);
+    productSet.add(product);
 }
 
-let acmeProducts = [new Product("Hat", 100), new Product("Boots", 100)];
-let zoomProducts = [new Product("Hat", 100), new Product("Boots", 100)];
-
-let products = new Map();
-
-[...acmeProducts, ...zoomProducts].forEach(p => products.set(p.id, p));
-
-let suppliers = new Map();
-suppliers.set("acme", new Supplier("Acme Co", acmeProducts.map(p => p.id)));
-suppliers.set("zoom", new Supplier("Zoom shoes", zoomProducts.map(p => p.id)));
-
-suppliers.get("acme").productids.forEach(id => console.log(`Name: ${products.get(id).name}`));
+console.log(`Array length: ${productArray.length}`);
+console.log(`Set size: ${productSet.size}`);
