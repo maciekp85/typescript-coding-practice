6d567d1a2f38859425ee63a725aca39aaa49afb1
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-29 11:30:09 +0100

    5.9-5.11 Understanding compiler errors

--- Chapter 05/tools/src/index.ts
+++ Chapter 05/tools/src/index.ts
@@ -1,5 +1,6 @@
-function printMessage(msg: String): void {
+function printMessage(msg: string): void {
     console.log(`Message: ${msg}`);
 }
 
 printMessage("Hello, TypeScript");
+// printMessage(100);   // creating a type mismatch
