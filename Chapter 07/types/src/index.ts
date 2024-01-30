f7e8d29fc4d429f9d7eb23cb6335e9de1243be97
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-30 15:43:01 +0100

    7.19 Using implicitly defined any types

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -1,4 +1,4 @@
-function calculateTax(amount: any): any {
+function calculateTax(amount): any {
     return `$${(amount * 1.2).toFixed(2)}`;
 }
 
@@ -6,10 +6,12 @@ let price= 100;
 let taxAmount = calculateTax(price);
 let halfShare = taxAmount / 2;
 
+let personVal = calculateTax("Bob");
+
 console.log(`Price: ${price}`);
 console.log(`Full amount in tax: ${taxAmount}`);
 console.log(`Half Share: ${halfShare}`);
 
 let newResult: any = calculateTax(200);
 let myNumber: number = newResult;
-console.log(`Number value: ${myNumber.toFixed(2)}`);
+console.log(`Name: ${personVal}`);
