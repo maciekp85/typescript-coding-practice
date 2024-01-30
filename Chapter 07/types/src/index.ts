6f9b5990262b078f56fab797979a92c269507080
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-30 15:27:13 +0100

    7.18 Using the any type. Assigning the any type

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -9,3 +9,7 @@ let halfShare = taxAmount / 2;
 console.log(`Price: ${price}`);
 console.log(`Full amount in tax: ${taxAmount}`);
 console.log(`Half Share: ${halfShare}`);
+
+let newResult: any = calculateTax(200);
+let myNumber: number = newResult;
+console.log(`Number value: ${myNumber.toFixed(2)}`);
