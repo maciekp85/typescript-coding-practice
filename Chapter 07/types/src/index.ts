28ceaeaea2755cbfe896220c8e30527288f8cccf
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-30 13:47:51 +0100

    7.12 Using implicitly defined static types. Relying on implicit types

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -1,14 +1,10 @@
-function calculateTax(amount: number): number {
+function calculateTax(amount: number) {
     return amount * 1.2;
 }
 
-let price: number = 100;
-let taxAmount: number = calculateTax(price);
-let halfShare: number = taxAmount / 2;
+let price= 100;
+let taxAmount = calculateTax(price);
+let halfShare = taxAmount / 2;
 
 console.log(`Full amount in tax: ${taxAmount}`);
 console.log(`Half Share: ${halfShare}`);
-
-// console.log(`${12} = ${calculateTax(12)}`);
-// console.log(`${"Hello"} = ${calculateTax("Hello")}`);
-// console.log(`${true} = ${calculateTax(true)}`);
