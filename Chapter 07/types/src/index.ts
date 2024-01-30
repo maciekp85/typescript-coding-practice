77525bd45ec4f5be296b2a22ad7cc77f10fcbffb
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-30 10:06:40 +0100

    7.7-7.8 Understanding static types. Displaying the variable type

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, TypeScript");
+let myVar;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = 12;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = "Hello";
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = true;
+console.log(`${myVar} = ${typeof myVar}`);
