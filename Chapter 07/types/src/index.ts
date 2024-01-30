790ed9b9d4d33f32a627c9f3012545e04eba16d9
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-30 11:11:03 +0100

    7.10-7.11 Creating a static type with a type annotation

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -1,7 +1,14 @@
-function calculateTax(amount) {
+function calculateTax(amount: number): number {
     return amount * 1.2;
 }
 
-console.log(`${12} = ${calculateTax(12)}`);
-console.log(`${"Hello"} = ${calculateTax("Hello")}`);
-console.log(`${true} = ${calculateTax(true)}`);
+let price: number = 100;
+let taxAmount: number = calculateTax(price);
+let halfShare: number = taxAmount / 2;
+
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half Share: ${halfShare}`);
+
+// console.log(`${12} = ${calculateTax(12)}`);
+// console.log(`${"Hello"} = ${calculateTax("Hello")}`);
+// console.log(`${true} = ${calculateTax(true)}`);
