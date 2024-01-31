1b5069f0540672dcbaf27cfeb22b90b3305d0cd2
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-31 13:03:06 +0100

    7.35 Removing null from a union with a type guard

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -5,22 +5,21 @@ function calculateTax(amount: number, format: boolean): string | number | null {
     const calcAmount = amount * 1.2;
     return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
 }
-let taxValue: string | number = calculateTax(100, false)!;
+let taxValue: string | number | null = calculateTax(100, false)!;
 
-switch(typeof taxValue) {
-    case "number": {
-        console.log(`Number Value: ${taxValue.toFixed(2)}`);
-        break;        
-    }
-    case "string": {
-        console.log(`String Value: ${taxValue.charAt(0)}`);
-        break;
-    }
-    default:
-        if (taxValue === null) {
-            console.log(`Value is null`);
-        } else {
-            let value: never = taxValue;
-            console.log(`Unexpected type for value: ${value}`);
+if (taxValue !== null) {
+    let notNullTaxValue: string | number = taxValue;
+
+    switch(typeof taxValue) {
+        case "number": {
+            console.log(`Number Value: ${taxValue.toFixed(2)}`);
+            break;        
         }
+        case "string": {
+            console.log(`String Value: ${taxValue.charAt(0)}`);
+            break;
+        }
+    }
+} else {
+    console.log(`Value is not string or a number`);
 }
