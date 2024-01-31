fb67d9d6610b9dd10d7acf9f17cf6dcf6feeb987
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-31 16:35:55 +0100

    8.18 Defining void functions

--- Chapter 08/types/src/index.ts
+++ Chapter 08/types/src/index.ts
@@ -2,5 +2,8 @@ function calculateTax(amount: number, discount: number = 0, ...extraFees: number
     return (amount * 1.2) - discount + extraFees.reduce((total, val) => total += val, 0);
 }
 
-let taxValue = calculateTax(100, 0);
-console.log(`Tax Value: ${taxValue}`);
+function writeValues(label: string, value: number): void {
+    console.log(`${label}: ${value}`);
+}
+
+writeValues("Tax value", calculateTax(100, 0));
