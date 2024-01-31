e6b06a126e5422add368102fc6eb4d4a7e459fcc
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-31 10:58:33 +0100

    7.27 Type guarding in a switch statement

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -5,8 +5,13 @@ function calculateTax(amount: number, format: boolean): string | number {
 
 let taxValue = calculateTax(100, false);
 
-if (typeof taxValue === "number") {
-    console.log(`Number Value: ${taxValue.toFixed(2)}`);
-} else if(typeof taxValue === "string") {
-    console.log(`String value: ${taxValue.charAt(0)}`);
+switch(typeof taxValue) {
+    case "number": {
+        console.log(`Number Value: ${taxValue.toFixed(2)}`);
+        break;        
+    }
+    case "string": {
+        console.log(`String Value: ${taxValue.charAt(0)}`);
+        break;
+    }
 }
