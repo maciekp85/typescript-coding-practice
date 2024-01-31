c3c34b044011dc447256358aa971057219b6debd
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-31 13:08:56 +0100

    7.36-7.37 Using the definite assignment assertion

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -5,7 +5,9 @@ function calculateTax(amount: number, format: boolean): string | number | null {
     const calcAmount = amount * 1.2;
     return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
 }
-let taxValue: string | number | null = calculateTax(100, false)!;
+
+let taxValue!: string | number | null;
+eval("taxValue = calculateTax(100, false)");
 
 if (taxValue !== null) {
     let notNullTaxValue: string | number = taxValue;
