cfa850ed334c3fff32958f0957a5b8843d9ecfa3
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-31 10:15:48 +0100

    7.25 Asserting to an unexpected type. Preventing the compiler error

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -5,7 +5,7 @@ function calculateTax(amount: number, format: boolean): string | number {
 
 let taxNumber = calculateTax(100, false) as number;
 let taxString = calculateTax(100, true) as string;
-let taxBoolean = calculateTax(100, false) as boolean;
+let taxBoolean = calculateTax(100, false) as any as boolean;
 
 console.log(`Number Value: ${taxNumber.toFixed(2)}`);
 console.log(`String value: ${taxString.charAt(0)}`);
