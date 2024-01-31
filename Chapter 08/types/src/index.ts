e292a02ff6680902387f3947d55f6a8ec3e1a510
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-31 15:13:34 +0100

    8.6 Understanding function parameters

--- Chapter 08/types/src/index.ts
+++ Chapter 08/types/src/index.ts
@@ -3,4 +3,8 @@ function calculateTax(amount, discount) {
 }
 
 let taxValue = calculateTax(100, 0);
-console.log(`Total Amount: ${taxValue}`);
+console.log(`2 args: ${taxValue}`);
+taxValue = calculateTax(100);
+console.log(`1 arg: ${taxValue}`);
+taxValue = calculateTax(100, 10, 20);
+console.log(`3 args: ${taxValue}`);
