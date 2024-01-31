9bc2db84e03f9cd2b1f6904ffd7c8830c3e5d522
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-01-31 10:07:45 +0100

    7.23 Using type assertions

--- Chapter 07/types/src/index.ts
+++ Chapter 07/types/src/index.ts
@@ -3,8 +3,8 @@ function calculateTax(amount: number, format: boolean): string | number {
     return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
 }
 
-let taxNumber : string | number = calculateTax(100, false);
-let taxString : string | number = calculateTax(100, true);
+let taxNumber : number = calculateTax(100, false) as number;
+let taxString : string = calculateTax(100, true) as string;
 
 console.log(`Number Value: ${taxNumber.toFixed(2)}`);
 console.log(`String value: ${taxString.charAt(0)}`);
