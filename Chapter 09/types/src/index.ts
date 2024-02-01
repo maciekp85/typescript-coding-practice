0489c39f1dfb37bfcc7881b47719187fa658be4f
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-01 16:47:10 +0100

    9.32 Using template literal string types

--- Chapter 09/types/src/index.ts
+++ Chapter 09/types/src/index.ts
@@ -9,26 +9,9 @@ function getRandomValue(): 1 | 2 | 3 | 4 {
     return Math.floor(Math.random() * 4) + 1 as 1 | 2 | 3 | 4;
 }
 
-enum City { London = "LON", Paris = "PAR", Chicago = "CHI"}
-
-function getMixedValue(input: 1): 1;
-function getMixedValue(input: 2 | 3): "Hello" | true;
-function getMixedValue(input: 4): City.London;
-function getMixedValue(input: number): 1 | "Hello" | true | City.London {
-    switch(input) {
-        case 1:
-            return 1;
-        case 2:
-            return "Hello";
-        case 3:
-            return true;
-        case 4:
-        default:
-            return City.London;
-    }
+function getCityString(city: "London" | "Paris" | "Chicago"): `City: ${"London" | "Paris" | "Chicago"}` {
+    return `City: ${city}`;
 }
 
-let first = getMixedValue(1);
-let second = getMixedValue(2);
-let third = getMixedValue(4);
-console.log(`${first}, ${second}, ${third}`);
+let str = getCityString("London");
+console.log(str);
