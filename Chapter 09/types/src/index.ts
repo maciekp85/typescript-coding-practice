0d6dcf7a094d829bb96847aa29ff56bad707727d
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-01 11:57:42 +0100

    9.12 Destructuring tuples

--- Chapter 09/types/src/index.ts
+++ Chapter 09/types/src/index.ts
@@ -9,13 +9,6 @@ function writePrice(product: string, price: number): void {
 let hat: [string, number] = ["Hat", 100];
 let gloves: [string, number] = ["Gloves", 75];
 
-writePrice(hat[0], hat[1]);
-writePrice(gloves[0], gloves[1]);
-
-hat.forEach((h: string | number) => {
-    if (typeof h === "string") {
-        console.log(`String: ${h}`);
-    } else {
-        console.log(`Number: ${h.toFixed(2)}`);
-    }
-})
+let [hatname, hatprice] = hat;
+console.log(`Name: ${hatname}`);
+console.log(`Price: ${hatprice.toFixed(2)}`);
