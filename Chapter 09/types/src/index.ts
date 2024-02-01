62179f99779c1f9ae554e79624c039fc764acf52
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-01 16:42:15 +0100

    9.31 Using overrides with literal value types

--- Chapter 09/types/src/index.ts
+++ Chapter 09/types/src/index.ts
@@ -11,8 +11,11 @@ function getRandomValue(): 1 | 2 | 3 | 4 {
 
 enum City { London = "LON", Paris = "PAR", Chicago = "CHI"}
 
-function getMixedValue(): 1 | "Hello" | true | City.London {
-    switch(getRandomValue()) {
+function getMixedValue(input: 1): 1;
+function getMixedValue(input: 2 | 3): "Hello" | true;
+function getMixedValue(input: 4): City.London;
+function getMixedValue(input: number): 1 | "Hello" | true | City.London {
+    switch(input) {
         case 1:
             return 1;
         case 2:
@@ -20,8 +23,12 @@ function getMixedValue(): 1 | "Hello" | true | City.London {
         case 3:
             return true;
         case 4:
+        default:
             return City.London;
     }
 }
 
-console.log(getMixedValue());
+let first = getMixedValue(1);
+let second = getMixedValue(2);
+let third = getMixedValue(4);
+console.log(`${first}, ${second}, ${third}`);
