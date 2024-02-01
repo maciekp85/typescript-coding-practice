8a66ff7e41a334e98007c5ffd8b16b2fc9fa5edb
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-01 12:14:46 +0100

    9.15 Defining tuples with rest elements

--- Chapter 09/types/src/index.ts
+++ Chapter 09/types/src/index.ts
@@ -6,13 +6,14 @@ function writePrice(product: string, price: number): void {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 
-let hat: [string, number, number?] = ["Hat", 100];
-let gloves: [string, number, number?] = ["Gloves", 75, 10];
+let hat: [string, number, number?, ...number[]] = ["Hat", 100, 10, 1.20, 3, 0.95];
+let gloves: [string, number, number?, ...number[]] = ["Gloves", 75, 10];
 
 [hat, gloves].forEach(tuple => {
-    let [name, price, taxRate] = tuple;
+    let [name, price, taxRate, ...coupons] = tuple;
     if (taxRate != undefined) {
         price += price * (taxRate/100);
     }
+    coupons.forEach(c => price -= c);
     writePrice(name, price);
 })
