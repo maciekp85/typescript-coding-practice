3d781d6d7ff375410de48cba40b8065822438cc0
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-01 16:04:07 +0100

    9.28 Defining a second literal value type

--- Chapter 09/types/src/index.ts
+++ Chapter 09/types/src/index.ts
@@ -1,2 +1,8 @@
-let restrictedValue: 1 | 2 | 3 = 100;
+let restrictedValue: 1 | 2 | 3 = 1;
+
+let secondValue: 1 | 10 | 100 = 1;
+
+restrictedValue = secondValue;
+secondValue = 100;
+restrictedValue = secondValue;
 console.log(`Value: ${restrictedValue}`);
