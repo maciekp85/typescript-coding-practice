bb14e858d0474f5f71191c0615fe3e42e4e6ba80
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-01 12:05:07 +0100

    9.13 Using tuple tuples

--- Chapter 09/types/src/index.ts
+++ Chapter 09/types/src/index.ts
@@ -9,6 +9,15 @@ function writePrice(product: string, price: number): void {
 let hat: [string, number] = ["Hat", 100];
 let gloves: [string, number] = ["Gloves", 75];
 
-let [hatname, hatprice] = hat;
-console.log(`Name: ${hatname}`);
-console.log(`Price: ${hatprice.toFixed(2)}`);
+let products: [string, number][] = [["Hat", 100], ["Gloves", 75]];
+let tupleUnion: ([string, number] | boolean)[] = [true, false, hat, ...products];
+
+tupleUnion.forEach((elem: [string, number] | boolean) => {
+    if (elem instanceof Array) {
+        let [str, num] = elem;
+        console.log(`Name: ${str}`);
+        console.log(`Price: ${num.toFixed(2)}`);
+    } else if (typeof elem === "boolean") {
+        console.log(`Boolean Value: ${elem}`);
+    }
+})
