5185886d60359f0d6044701e3802a81dba805655
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-01 14:38:27 +0100

    9.20 Using expressions in an enum

--- Chapter 09/types/src/index.ts
+++ Chapter 09/types/src/index.ts
@@ -6,8 +6,9 @@ function writePrice(product: string, price: number): void {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 
-enum Product { Hat, Gloves = 20, Umbrella }
+enum OtherEnum { First = 10, Two = 20 }
+enum Product { Hat = OtherEnum.First + 1, Gloves = 20, Umbrella = Hat + Gloves }
 
-let productValue: Product = 0;
+let productValue: Product = 11;
 let productName: string = Product[productValue];
 console.log(`Value: ${productValue}, Name: ${productName}`);
