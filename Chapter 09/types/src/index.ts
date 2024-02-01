8cf320e624e59f71d3577372ee00dd268a7697ab
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-01 12:10:24 +0100

    9.14 Using tuples with optional elements

--- Chapter 09/types/src/index.ts
+++ Chapter 09/types/src/index.ts
@@ -6,18 +6,13 @@ function writePrice(product: string, price: number): void {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 
-let hat: [string, number] = ["Hat", 100];
-let gloves: [string, number] = ["Gloves", 75];
+let hat: [string, number, number?] = ["Hat", 100];
+let gloves: [string, number, number?] = ["Gloves", 75, 10];
 
-let products: [string, number][] = [["Hat", 100], ["Gloves", 75]];
-let tupleUnion: ([string, number] | boolean)[] = [true, false, hat, ...products];
-
-tupleUnion.forEach((elem: [string, number] | boolean) => {
-    if (elem instanceof Array) {
-        let [str, num] = elem;
-        console.log(`Name: ${str}`);
-        console.log(`Price: ${num.toFixed(2)}`);
-    } else if (typeof elem === "boolean") {
-        console.log(`Boolean Value: ${elem}`);
+[hat, gloves].forEach(tuple => {
+    let [name, price, taxRate] = tuple;
+    if (taxRate != undefined) {
+        price += price * (taxRate/100);
     }
+    writePrice(name, price);
 })
