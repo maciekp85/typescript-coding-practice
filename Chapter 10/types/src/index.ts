7ed9d17f6ce90f220585b3591de1dfe40a96aba6
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-02 10:08:44 +0100

    10.7 Using optional properties for irregular shapes

--- Chapter 10/types/src/index.ts
+++ Chapter 10/types/src/index.ts
@@ -2,6 +2,6 @@ let hat = { name: "Hat", price: 100 };
 let gloves = { name: "Gloves", price: 75};
 let umbrella = { name: "Umbrella", price: 30, waterproof: true };
 
-let products: { name: string, price?: number}[] = [hat, gloves, umbrella];
+let products: { name: string, price?: number, waterproof?: boolean}[] = [hat, gloves, umbrella];
 
-products.forEach(prod => console.log(`${prod.name}: ${prod.price}`));
+products.forEach(prod => console.log(`${prod.name}: ${prod.price} ` + `Waterproof: ${ prod.waterproof}`));
