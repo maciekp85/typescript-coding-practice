d891a32486e48dee168e55b817023e4626d532b9
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-02 11:43:02 +0100

    10.13 Understanding union property types

--- Chapter 10/types/src/index.ts
+++ Chapter 10/types/src/index.ts
@@ -10,11 +10,16 @@ type Person = {
     city: string
 };
 
+type UnionType = {
+    id: number | string,
+    name: string
+};
+
 let hat = { id: 1, name: "Hat", price: 100 };
 let gloves = { id: 2, name: "Gloves", price: 75 };
 let umbrella = { id: 3, name: "Umbrella", price: 30 };
 let bob = { id: "bsmith", name: "Bob", city: "London" };
 
-let dataItems: (Product | Person)[] = [hat, gloves, umbrella, bob];
+let dataItems: UnionType[] = [hat, gloves, umbrella, bob];
 
 dataItems.forEach(item => console.log(`ID: ${item.id}, Name: ${item.name}`));
