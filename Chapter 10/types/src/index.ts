3f6a9b35ebe6115df3e898b62931f425f30bce99
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-02 13:51:40 +0100

    10.21 Merging properties with different types. Assigning values to the intersection

--- Chapter 10/types/src/index.ts
+++ Chapter 10/types/src/index.ts
@@ -16,3 +16,11 @@ type EmployedPerson = Person & Employee;
 
 let typeTest = ({} as EmployedPerson).contact;
 
+let person1: EmployedPerson = {
+    id: "bsmith", name: "Bob Smith", city: "London", company: "Acme Co", dept: "Sales", contact: "Alice"
+}
+
+
+let person2: EmployedPerson = {
+    id: "dpeters", name: "Dore Peters", city: "New York", company: "Acme Co", dept: "Development", contact: 6512346543
+}
