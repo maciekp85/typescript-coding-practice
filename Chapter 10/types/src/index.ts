e224ef1b6f6959359d6f21c0ab0cf3952da670b4
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-02 10:34:13 +0100

    10.8 Including methods in shape types

--- Chapter 10/types/src/index.ts
+++ Chapter 10/types/src/index.ts
@@ -1,7 +1,26 @@
-let hat = { name: "Hat", price: 100 };
-let gloves = { name: "Gloves", price: 75};
-let umbrella = { name: "Umbrella", price: 30, waterproof: true };
+enum Feature { Waterproof, Insulated }
 
-let products: { name: string, price?: number, waterproof?: boolean}[] = [hat, gloves, umbrella];
+let hat = { 
+    name: "Hat", 
+    price: 100 
+};
 
-products.forEach(prod => console.log(`${prod.name}: ${prod.price} ` + `Waterproof: ${ prod.waterproof}`));
+let gloves = { 
+    name: "Gloves",
+    price: 75 
+};
+
+let umbrella = { 
+    name: "Umbrella", 
+    price: 30, 
+    hasFeature: (feature) => feature === Feature.Waterproof 
+};
+
+let products: { 
+    name: string, 
+    price?: number, 
+    hasFeature?(Feature): boolean 
+}[] = [hat, gloves, umbrella];
+
+products.forEach(prod => console.log(`${prod.name}: ${prod.price} ` + 
+    `Waterproof: ${ prod.hasFeature(Feature.Waterproof)}`));
