f740da3e700604f15bb72d3b537691e1c33d3950
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-02 09:34:07 +0100

    10.6 Understanding how shape types fit. Adding properties

--- Chapter 10/types/src/index.ts
+++ Chapter 10/types/src/index.ts
@@ -1,7 +1,7 @@
 let hat = { name: "Hat", price: 100 };
 let gloves = { name: "Gloves", price: 75};
-let umbrella = { name: "Umbrella"};
+let umbrella = { name: "Umbrella", price: 30, waterproof: true };
 
-let products: { name: string, price: number}[] = [hat, gloves, umbrella];
+let products: { name: string, price?: number}[] = [hat, gloves, umbrella];
 
 products.forEach(prod => console.log(`${prod.name}: ${prod.price}`));
