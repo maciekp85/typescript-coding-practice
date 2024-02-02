3e05a33a6e45f51bb5fc935c499df67c048ec183
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-02 11:46:22 +0100

    10.14 Using type guards for objects. Type guarding

--- Chapter 10/types/src/index.ts
+++ Chapter 10/types/src/index.ts
@@ -20,6 +20,6 @@ let gloves = { id: 2, name: "Gloves", price: 75 };
 let umbrella = { id: 3, name: "Umbrella", price: 30 };
 let bob = { id: "bsmith", name: "Bob", city: "London" };
 
-let dataItems: UnionType[] = [hat, gloves, umbrella, bob];
+let dataItems: (Product | Person)[] = [hat, gloves, umbrella, bob];
 
-dataItems.forEach(item => console.log(`ID: ${item.id}, Name: ${item.name}`));
+dataItems.forEach(item => console.log(`ID: ${item.id}, Type: ${typeof item}`));
