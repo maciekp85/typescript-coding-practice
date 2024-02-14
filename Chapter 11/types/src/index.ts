65a713844f9b712e0c994e89b78bd6b086fe5586
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-14 09:49:10 +0100

    11.18 Understanding type inference for subclasses. Using a common superclass

--- Chapter 11/types/src/index.ts
+++ Chapter 11/types/src/index.ts
@@ -6,8 +6,6 @@ class Person {
 
 class Employee extends Person {
 
-    // private city: string;
-
     constructor(public readonly id: string, public name: string, private dept: string, city: string) {
         super(id, name, city);
         this.city = city;
@@ -16,36 +14,34 @@ class Employee extends Person {
     writeDept(): void {
         console.log(`${this.name} works in ${this.dept}`);
     }
-
-//     get location() {
-//         switch(this.city) {
-//             case "Paris":
-//                 return "France";
-//             case "London":
-//                 return "UK";
-//             default:
-//                 return this.city;
-//         }
-//     }
-
-//     get details() {
-//         return `${this.name}, ${this.dept}, ${this.location}`;
-//     }
-
-//     accessor salary: number = 100_000;
 }
 
-// let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
-// salesEmployee.writeDept();
-// console.log(`Location: ${salesEmployee.location}`);
-// console.log(`Details: ${salesEmployee.details}`);
-// console.log(`Salary: ${salesEmployee.salary}`);
+class Customer extends Person {
 
-let data = [new Person("bsmith", "Bob Smith", "London"), new Employee("fvega", "Fidel Vega", "Sales", "Paris")];
+    constructor(public readonly id: string, public name: string, public city: string, public creditLimit: number) {
+        super(id, name, city);
+    }
+}
+
+class Supplier extends Person {
+
+    constructor(public readonly id: string, public name: string, public city: string, public companyName: string) {
+        super(id, name, city);
+    }
+}
+
+
+let data = [new Employee("fvega", "Fidel Vega", "Sales", "Paris"), new Customer("ajones", "Alice Jones", "London", 500)];
+
+data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
 
 data.forEach(item => {
     console.log(`Person: ${item.name}, ${item.city}`);
     if (item instanceof Employee) {
         item.writeDept();
+    } else if (item instanceof Customer) {
+        console.log(`Customer ${item.name} has ${item.creditLimit} limit`);
+    } else if (item instanceof Supplier) {
+        console.log(`Customer ${item.name} works for ${item.companyName}`);
     }
 })
