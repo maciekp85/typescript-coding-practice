246fabb5a3864aed1708155ce5016c9f19c17ab3
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-14 09:39:11 +0100

    11.17 Using class inheritance

--- Chapter 11/types/src/index.ts
+++ Chapter 11/types/src/index.ts
@@ -1,15 +1,15 @@
-type Person = {
-    id: string,
-    name: string,
-    city: string
-};
+class Person {
+    constructor(public id: string, public name: string, public city: string) {
 
-class Employee {
+    }
+}
 
-    private city: string;
+class Employee extends Person {
+
+    // private city: string;
 
     constructor(public readonly id: string, public name: string, private dept: string, city: string) {
-        // no statements required
+        super(id, name, city);
         this.city = city;
     }
 
@@ -17,26 +17,35 @@ class Employee {
         console.log(`${this.name} works in ${this.dept}`);
     }
 
-    get location() {
-        switch(this.city) {
-            case "Paris":
-                return "France";
-            case "London":
-                return "UK";
-            default:
-                return this.city;
-        }
-    }
+//     get location() {
+//         switch(this.city) {
+//             case "Paris":
+//                 return "France";
+//             case "London":
+//                 return "UK";
+//             default:
+//                 return this.city;
+//         }
+//     }
 
-    get details() {
-        return `${this.name}, ${this.dept}, ${this.location}`;
-    }
+//     get details() {
+//         return `${this.name}, ${this.dept}, ${this.location}`;
+//     }
 
-    accessor salary: number = 100_000;
+//     accessor salary: number = 100_000;
 }
 
-let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
-salesEmployee.writeDept();
-console.log(`Location: ${salesEmployee.location}`);
-console.log(`Details: ${salesEmployee.details}`);
-console.log(`Salary: ${salesEmployee.salary}`);
+// let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
+// salesEmployee.writeDept();
+// console.log(`Location: ${salesEmployee.location}`);
+// console.log(`Details: ${salesEmployee.details}`);
+// console.log(`Salary: ${salesEmployee.salary}`);
+
+let data = [new Person("bsmith", "Bob Smith", "London"), new Employee("fvega", "Fidel Vega", "Sales", "Paris")];
+
+data.forEach(item => {
+    console.log(`Person: ${item.name}, ${item.city}`);
+    if (item instanceof Employee) {
+        item.writeDept();
+    }
+})
