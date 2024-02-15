2c3b88a23d5fb6796c3fc397151c0cdefb9aa146
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-15 14:03:44 +0100

    12.18 Narrowing a generic type

--- Chapter 12/types/src/index.ts
+++ Chapter 12/types/src/index.ts
@@ -5,40 +5,18 @@ let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 let cities = [new City("London", 8136000), new City("Paris", 2141000)];
 let employees = [new Employee("Bob Smith", "Sales"), new Employee("Alice Jones", "Sales")];
 
-class DataCollection<T extends {name: string}> {
+class DataCollection<T> {
     protected items: T[] = [];
 
     constructor(initialItems: T[]) {
         this.items.push(...initialItems);
     }
 
-    collate<U>(targetData: U[], itemProp: string, targetProp: string): (T & U)[] {
-        let results = [];
-        this.items.forEach(item => {
-            let match = targetData.find(d => d[targetProp] === item[itemProp]);
-            if (match !== undefined) {
-                results.push({...match, ...item});
-            }
-        });
-        return results;
+    filter<V extends T>(): V[] {
+        return this.items.filter(item => item instanceof V) as V[];
     }
 }
 
-class SearchableCollection<T extends Employee | Person> extends DataCollection<T> {
-    constructor(initialItems: T[]) {
-        super(initialItems);
-    }
-
-    find(searchTerm: string): T[] {
-        return this.items.filter(item => {
-            if (item instanceof Employee) {
-                return item.name === searchTerm || item.role === searchTerm;
-            } else if ( item instanceof Person) {
-                return item.name === searchTerm || item.city == searchTerm;
-            }
-        });
-    }
- }
-
-let employeeData = new SearchableCollection<Employee>(employees);
-employeeData.find("Sales").forEach(e => console.log(`Employee ${e.name}, ${e.role}`));
+let mixedData = new DataCollection<Person | Product>([...people, ...products]);
+let filteredProducts = mixedData.filter<Product>();
+filteredProducts.forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
