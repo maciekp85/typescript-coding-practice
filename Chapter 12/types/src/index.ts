b6e2f8a55c89f9e0960fe10615f839da1446ac47
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-15 10:39:38 +0100

    12.10 Constraining generic type values

--- Chapter 12/types/src/index.ts
+++ Chapter 12/types/src/index.ts
@@ -3,7 +3,7 @@ import { Person, Product } from "./dataTypes.js";
 let people = [ new Person("Bob Smith", "London"), new Person("Dora Peters", "New York")];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 
-class DataCollection<T> {
+class DataCollection<T extends (Person | Product)> {
     private items: T[] = [];
 
     constructor(initialItems: T[]) {
@@ -14,9 +14,9 @@ class DataCollection<T> {
         this.items.push(newItem);
     }
 
-    // getNames(): string[] {
-    //     return this.items.map(item => item.name);
-    // }
+    getNames(): string[] {
+        return this.items.map(item => item.name);
+    }
 
     getItem(index: number): T {
         return this.items[index];
@@ -26,8 +26,10 @@ class DataCollection<T> {
 let peopleData = new DataCollection<Person>(people);
 let firstPerson = peopleData.getItem(0);
 console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
+console.log(`Person Names: ${peopleData.getNames().join(", ")}`);
 
 
 let productData = new DataCollection<Product>(products);
 let firstProduct = productData.getItem(0);
 console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
+console.log(`Product Names: ${productData.getNames().join(", ")}`);
