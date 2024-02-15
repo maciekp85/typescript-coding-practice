26eaa3d4d46177e96c4092d4fd67222f6b6be3b9
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-15 11:39:05 +0100

    12.13 Applying a type parameter to a method

--- Chapter 12/types/src/index.ts
+++ Chapter 12/types/src/index.ts
@@ -1,17 +1,18 @@
-import { City, Person, Product } from "./dataTypes.js";
+import { City, Person, Product, Employee } from "./dataTypes.js";
 
 let people = [ new Person("Bob Smith", "London"), new Person("Dora Peters", "New York")];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 let cities = [new City("London", 8136000), new City("Paris", 2141000)];
+let employees = [new Employee("Bob Smith", "Sales"), new Employee("Alice Jones", "Sales")];
 
-class DataCollection<T extends {name: string}, U> {
+class DataCollection<T extends {name: string}> {
     private items: T[] = [];
 
     constructor(initialItems: T[]) {
         this.items.push(...initialItems);
     }
 
-    collate(targetData: U[], itemProp: string, targetProp: string): (T & U)[] {
+    collate<U>(targetData: U[], itemProp: string, targetProp: string): (T & U)[] {
         let results = [];
         this.items.forEach(item => {
             let match = targetData.find(d => d[targetProp] === item[itemProp]);
@@ -23,6 +24,8 @@ class DataCollection<T extends {name: string}, U> {
     }
 }
 
-let peopleData = new DataCollection<Person, City>(people);
-let collatedData = peopleData.collate(cities, "city", "name");
+let peopleData = new DataCollection<Person>(people);
+let collatedData = peopleData.collate<City>(cities, "city", "name");
 collatedData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.population}`));
+let empData = peopleData.collate<Employee>(employees, "name", "name");
+empData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.role}`));
