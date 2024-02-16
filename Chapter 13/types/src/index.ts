01016f34aaf5541a7e8cb7ac4509d442bf519e6e
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-16 17:47:45 +0100

    13.3 Using generic collections. Set

--- Chapter 13/types/src/index.ts
+++ Chapter 13/types/src/index.ts
@@ -6,18 +6,22 @@ type shapeType = { name: string };
 
 class Collection<T extends shapeType> {
 
-    constructor(private items: T[] = []) {}
+    private items: Set<T>;
+
+    constructor(initialItems: T[] = []) {
+        this.items = new Set<T>(initialItems);
+    }
 
     add(...newItems: T[]): void {
-        this.items.push(...newItems);
+        newItems.forEach(newItem => this.items.add(newItem));
     }
 
     get(name: string): T {
-        return this.items.find(item => item.name === name);
+        return [...this.items.values()].find(item => item.name === name);
     }
 
     get count(): number {
-        return this.items.length;
+        return this.items.size;
     }
 }
 
