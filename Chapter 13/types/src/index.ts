af9a434d5a2df01da52f92cb07de7b625a340d37
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-19 14:28:15 +0100

    13.26 Using conditional types with type unions. Filtering a type union

--- Chapter 13/types/src/index.ts
+++ Chapter 13/types/src/index.ts
@@ -1,23 +1,16 @@
 import { City, Person, Product, Employee } from "./dataTypes.js";
 
-type resultType<T extends boolean> = T extends true ? string : number;
+type Filter<T, U> = T extends U ? never: T;
 
-class Collection<T> {
-    private items: T[];
-
-    constructor(...initialItems: T[]) {
-        this.items = initialItems || [];
-    }
-
-    total<P extends keyof T, U extends boolean>(propName: P, format: U): resultType<U> {
-        let totalValue = this.items.reduce((t, item) => t += Number(item[propName]), 0);
-        return format ? `$${totalValue.toFixed()}` : totalValue as any;
-    }
+function FilterArray<T, U>(data: T[], predicate: (item) => item is U): Filter<T, U>[] {
+    return data.filter(item => !predicate(item)) as any;
 }
 
-let data = new Collection<Product>(new Product("Kayak", 275), new Product("Lifejacket", 48.95));
+let dataArray = [new Product("Kayak", 275), new Person("Bob", "London"), new Product("Lifejacket", 48.95)]
 
-let firstVal: string = data.total("price", true);
-console.log(`Formatted value: ${firstVal}`);
-let secondVal: number = data.total("price", false);
-console.log(`Unformatted value: ${secondVal}`);
+function isProduct(item: any): item is Product {
+    return item instanceof Product;
+}
+
+let filteredData: Person[] = FilterArray(dataArray, isProduct);
+filteredData.forEach(item => console.log(`Person: ${item.name}`));
