dd9a39ac44edc7a6cd577255cd375afd01e00b77
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-19 10:17:54 +0100

    13.11 Using the indexed access operator

--- Chapter 13/types/src/index.ts
+++ Chapter 13/types/src/index.ts
@@ -4,6 +4,9 @@ function getValue<T, K extends keyof T>(item: T, keyname: K) {
     console.log(`Value: ${item[keyname]}`);
 }
 
+type priceType = Product["price"];
+type allTypes = Product[keyof Product];
+
 let p = new Product("Running Shoes", 100);
 getValue<Product, "name">(p, "name");
 getValue(p, "price");
