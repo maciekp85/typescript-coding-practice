a5448e910f9b754a1f90405f9c787a83af2bcf64
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-19 10:58:30 +0100

    13.18 Changing property optionality and mutability

--- Chapter 13/types/src/index.ts
+++ Chapter 13/types/src/index.ts
@@ -1,11 +1,25 @@
 import { City, Person, Product, Employee } from "./dataTypes.js";
 
-type Mapped<T> = {
-    [P in keyof T]: T[P]
-};
+type MakeOptional<T> = {
+    [P in keyof T]? : T[P]
+}
 
-let p: Mapped<Product> = { name: "Kayak", price: 275};
+type MakeRequired<T> = {
+    [P in keyof T]-? : T[P]
+}
+
+type MakeReadOnly<T> = {
+    readonly [P in keyof T] : T[P]
+}
+
+type MakeReadWrite<T> = {
+    -readonly [P in keyof T] : T[P]
+}
+
+type optionalType = MakeOptional<Product>;
+type requiredType = MakeRequired<Product>;
+type readOnlyType = MakeReadOnly<Product>;
+type readWriteType = MakeReadWrite<Product>;
+
+let p: readWriteType = { name: "Kayak", price: 275};
 console.log(`Mapped type: ${p.name}, ${p.price}`);
-
-let c: Mapped<City> = { name: "London", population: 8136000};
-console.log(`Mapped type: ${c.name}, ${c.population}`);
