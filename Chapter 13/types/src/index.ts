ef8d41314bacbd2abc95dfb4bee0a0801615fc2f
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-19 13:46:07 +0100

    13.24 Nesting conditional types

--- Chapter 13/types/src/index.ts
+++ Chapter 13/types/src/index.ts
@@ -2,7 +2,10 @@ import { City, Person, Product, Employee } from "./dataTypes.js";
 
 type resultType<T extends boolean> = T extends true ? string : number;
 
-let firstVal: resultType<true> = "String Value";
-let secondVal: resultType<false> = 100;
+type references = "London" | "Bob" | "Kayak";
 
-let mismatchCheck: resultType<false> = "String Value";
+type nestedType<T extends references> = T extends "London" ? "City" : T extends "Bob" ? "Person" : Product;
+
+let firstVal: nestedType<"London"> = new City("London", 8136000);
+let secondVal: nestedType<"Bob"> = new Person("Bob", "London");
+let thirdVal: nestedType<"Kayak"> = new Product("Kayak", 275)
