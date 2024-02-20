ec1a91d54866fdc2a418528a86dbd9fb6efbc0b3
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-20 12:36:36 +0100

    14.15 Using specific types in decorator. Constraining the result type

--- Chapter 14/decorators/src/methodDecorator.ts
+++ Chapter 14/decorators/src/methodDecorator.ts
@@ -1,8 +1,8 @@
-interface HasGetPrice {
-    getPrice(): number;
-}
+// interface HasGetPrice {
+//     getPrice(): number;
+// }
 
-export function time<This extends HasGetPrice, Args extends any[], Result>(method: (This, Args) => Result, ctx: ClassMethodDecoratorContext<This, (This, Args) => Result>) {
+export function time<This, Args extends any[], Result extends string | number>(method: (This, Args) => Result, ctx: ClassMethodDecoratorContext<This, (This, Args) => Result>) {
     const methodName = String(ctx.name);
     return function(this: This, ...args: Args): Result {
         const start = performance.now();
