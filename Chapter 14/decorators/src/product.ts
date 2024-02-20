59d592df4d28d8c0156bad16e1c0ca9551c3cfe7
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-20 12:11:54 +0100

    14.9-14.10 Understanding decorators. Decorating a method

--- Chapter 14/decorators/src/product.ts
+++ Chapter 14/decorators/src/product.ts
@@ -1,7 +1,10 @@
+import { time } from "./methodDecorator.js";
+
 export class Product {
     
     constructor(public name: string, public price: number) {}
 
+    @time
     getDetails(): string {
         return `Name: ${this.name}, Price: $${this.price}`;
     }
