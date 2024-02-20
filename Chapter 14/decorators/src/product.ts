af51ba0c80d830d7d1731a837260127786fa9b94
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-20 11:11:56 +0100

    14.1-14.8 Using decorators. Preparing for the chapter 14

---
+++ Chapter 14/decorators/src/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    
+    constructor(public name: string, public price: number) {}
+
+    getDetails(): string {
+        return `Name: ${this.name}, Price: $${this.price}`;
+    }
+}
