da7311a893a30c7ec743219cf0fe960151b5284b
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-20 12:47:11 +0100

    14.19-14.20 Adding and using a type guard

--- Chapter 14/decorators/src/classDecorator.ts
+++ Chapter 14/decorators/src/classDecorator.ts
@@ -1,10 +1,18 @@
 export function serialize<T extends new (...args: any) => any>(originalClass: T, ctx: ClassDecoratorContext) {
     const className = String(ctx.name);
 
-    return class extends originalClass {
+    return class extends originalClass implements Serializeable {
 
         serialize() {
             console.log(`${className}: ${JSON.stringify(this)}`);
         }
     };
+}
+
+export interface Serializeable {
+    serialize();
+}
+
+export function isSerializeable(target): target is Serializeable {
+    return typeof target.serialize === "function";
 }
--- Chapter 14/decorators/src/index.ts
+++ Chapter 14/decorators/src/index.ts
@@ -1,5 +1,6 @@
 import { City } from "./city.js";
 import { Product } from "./product.js";
+import { isSerializeable } from "./classDecorator.js";
 
 let city = new City("London", 8_982_000);
 let product = new Product("Kayak", 275);
@@ -7,4 +8,6 @@ let product = new Product("Kayak", 275);
 console.log(city.getSummary());
 console.log(product.getDetails());
 
-(product as any).serialize();
+if (isSerializeable(product)) {
+    product.serialize();
+}
