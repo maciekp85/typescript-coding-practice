257164c0a7b877cc08943f394a0fc42dcb9fb967
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-21 10:22:15 +0100

    14.45-14.46 Accumulating state data

--- Chapter 14/decorators/src/methodDecorator.ts
+++ Chapter 14/decorators/src/methodDecorator.ts
@@ -4,6 +4,15 @@ type Config = {
     replacement?: Function
 }
 
+const timings = new Map<string, {count: number, elapsed: number}>();
+
+export function writeTimes() {
+    [...timings.entries()].forEach(t => {
+        const average = (t[1].elapsed / t[1].count).toFixed(2);
+        console.log(`${t[0]}, count: ${t[1].count}, time: ${average}ms`);
+    });
+}
+
 export function time(config? : Config) {
     return function(method, ctx: ClassMethodDecoratorContext) {
         let start;
@@ -12,14 +21,27 @@ export function time(config? : Config) {
         return function(this, ...args: any[]) {
             const start = performance.now();
             let result;
+            // if (config?.time) {
+            //     console.log(`${method.name} started`);
+            // }
             if (config?.replacement) {
                 result = config.replacement.call(this, args);
             } else {
                 result = method.call(this, args);
             }
             if (config?.time) {
-                const duration = (performance.now() - start).toFixed(2);
-                console.log(`${methodName} ended ${duration} ms`);
+                // const duration = (performance.now() - start).toFixed(2);
+                const duration = (performance.now() - start);
+                // console.log(`${methodName} ended ${duration} ms`);
+                if (timings.has(methodName)) {
+                    const data = timings.get(methodName);
+                    data.count++;
+                    data.elapsed += duration;
+                } else {
+                    timings.set(methodName, {
+                        count: 1, elapsed: duration
+                    });
+                }
             }
             return result;
         }
