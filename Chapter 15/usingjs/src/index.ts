3e92d39edec47bcf0ee8740fecfa993157a6ce19
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-21 11:27:05 +0100

    15.9-15.10 Working with JavaScript. Using JavaScript functions

--- Chapter 15/usingjs/src/index.ts
+++ Chapter 15/usingjs/src/index.ts
@@ -1,5 +1,6 @@
 import { SportsProduct, SPORT } from "./product.js";
 import { Cart } from "./cart.js";
+import { sizeFormatter, costFormatter } from "./formatters.js";
 
 let kayak = new SportsProduct(1, "Kayak", 275, SPORT.Watersports);
 let hat = new SportsProduct(2, "Hat", 22.10, SPORT.Running);
@@ -12,3 +13,6 @@ cart.addProduct(hat, 2);
 
 console.log(`Cart has ${cart.itemCount} items`);
 console.log(`Cart value is $${cart.totalPrice.toFixed(2)}`);
+
+sizeFormatter("Cart", cart.itemCount);
+costFormatter("Cart", cart.totalPrice);
