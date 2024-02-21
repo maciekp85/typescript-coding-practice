6ac1eaf5c4bd453188010f384a5cbb2e56844d68
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-21 13:52:48 +0100

    15.16 Using comments to describe types. Using JSDoc

--- Chapter 15/usingjs/src/formatters.js
+++ Chapter 15/usingjs/src/formatters.js
@@ -4,6 +4,11 @@ export function sizeFormatter(thing, count) {
     writeMessage(`The ${thing} has {count} items`);
 }
 
+/**
+ * Format something that has a money value
+ * @param {string} thing - the name of items
+ * @param {number} cost - the value associated with the item
+ */
 export function costFormatter(thing, cost) {
     writeMessage(`The ${thing} costs $${cost.toFixed(2)}`, true);
 }
