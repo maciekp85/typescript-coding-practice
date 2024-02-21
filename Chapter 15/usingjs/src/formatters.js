5cf534caac60dd8b1a360de5d1c37462573ac1e0
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-21 13:55:33 +0100

    15.17 Using comments to describe types. Describing a type union

--- Chapter 15/usingjs/src/formatters.js
+++ Chapter 15/usingjs/src/formatters.js
@@ -1,16 +1,21 @@
 // @ts-nocheck
 
 export function sizeFormatter(thing, count) {
-    writeMessage(`The ${thing} has {count} items`);
+    writeMessage(`The ${thing} has ${count} items`);
 }
 
 /**
  * Format something that has a money value
  * @param {string} thing - the name of items
- * @param {number} cost - the value associated with the item
+ * @param {number | string} cost - the value associated with the item
  */
 export function costFormatter(thing, cost) {
-    writeMessage(`The ${thing} costs $${cost.toFixed(2)}`, true);
+    if (typeof cost === "number") {
+        writeMessage(`The ${thing} costs $${cost.toFixed(2)}`, true);
+    } else {
+        writeMessage(`The ${thing} costs $${cost}`);
+    }
+    
 }
 
 function writeMessage(message) {
