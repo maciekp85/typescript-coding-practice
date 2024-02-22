e624d819e2ba1da28821664d2d374f796e16794c
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-22 10:39:26 +0100

    15.31-15.33 Using packages that include type declarations.

--- Chapter 15/usingjs/src/index.ts
+++ Chapter 15/usingjs/src/index.ts
@@ -2,6 +2,7 @@ import { SportsProduct, SPORT } from "./product.js";
 import { Cart } from "./cart.js";
 import { sizeFormatter, costFormatter, writeMessage } from "./formatters.js";
 import debug from "debug";
+import chalk from "chalk";
 
 let kayak = new SportsProduct(1, "Kayak", 275, SPORT.Watersports);
 let hat = new SportsProduct(2, "Hat", 22.10, SPORT.Running);
@@ -22,3 +23,6 @@ writeMessage("Test message");
 let db = debug("Example App");
 db.enabled = true;
 db("Message: %s", "Test message");
+
+console.log(chalk.greenBright("Formatted message"));
+console.log(chalk.notAColor("Formatted message"));
