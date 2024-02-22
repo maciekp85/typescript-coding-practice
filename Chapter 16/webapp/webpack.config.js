6035eaccab143d4c33b1a1f01486491af1bcbd8f
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-22 11:14:59 +0100

    16.6-16.9 Adding a bundler

---
+++ Chapter 16/webapp/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.ts",
+    output: { filename: "bundle.js" },
+    resolve: { extensions: [".ts", ".js"] },
+    module: {
+        rules: [
+            {test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }
+        ]
+    }
+};
