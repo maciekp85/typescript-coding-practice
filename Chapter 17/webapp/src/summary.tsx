f4c18da0bee3ab8839f125431c63c960730e952e
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-23 17:38:23 +0100

    17.13- Completing the application and small issue fixes

--- Chapter 17/webapp/src/summary.tsx
+++ Chapter 17/webapp/src/summary.tsx
@@ -11,7 +11,7 @@ export class Summary {
         return <div className="m-2 text-center">
             <h2>Thanks!</h2>
             <p>Thanks for placing your order.</p>
-            <p>Your order is ${this.props.orderId}</p>
+            <p>Your order is #{this.props.orderId}</p>
             <p>We'll ship your goods as soon as possible.</p>
             <button className="btn btn-primary" onclick={this.props.callback}>
                 OK
