190fc8c925dac403b12815ac5cc9ea18b61475fd
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-02-24 18:23:43 +0100

    Fixed critical issue

--- Chapter 17/webapp/server.js
+++ Chapter 17/webapp/server.js
@@ -8,7 +8,7 @@ app.use("/", express.static("assets"));
 
 const router = jsonServer.router("data.json");
 app.use(jsonServer.bodyParser);
-app.use("api", (req, resp, next) => router(req, resp, next));
+app.use("/api", (req, resp, next) => router(req, resp, next));
 
 const port = process.argv[3] || 4000;
 app.listen(port, () => console.log(`Running on port ${port}`));
