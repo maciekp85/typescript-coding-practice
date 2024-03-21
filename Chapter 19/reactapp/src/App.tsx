8856df12f1adb20541d68cb912ab8f0178bc1b4d
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2024-03-21 12:40:55 +0100

    20.22-20.24 Implementing the HTTP API clients

--- Chapter 19/reactapp/src/App.tsx
+++ Chapter 19/reactapp/src/App.tsx
@@ -1,20 +1,24 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import { Product } from "./data/entities";
 import { ProductList } from './productList';
-import { useAppDispatch, useAppSelector, reducers } from './data/dataStore';
+import { useAppDispatch, useAppSelector, reducers, queries } from './data/dataStore';
 
-let testData: Product[] = [1, 2, 3, 4, 5].map(num => ({ id: num, name: `Prod${num}`, category: `Cat${num % 2}`, description: `Product ${num}`, price: 1000}))
+// let testData: Product[] = [1, 2, 3, 4, 5].map(num => ({ id: num, name: `Prod${num}`, category: `Cat${num % 2}`, description: `Product ${num}`, price: 1000}))
 
 export const App: FunctionComponent = () => {
   const selections = useAppSelector(state => state.selections);
   const dispatch = useAppDispatch();
 
+  const { data} = queries.useGetProductsQuery();
+
   const addToOrder = (p: Product, q: number) => dispatch(reducers.addToOrder([p, q]));
 
-  const categories = [...new Set(testData.map(p => p.category))];
+  const categories = useMemo<string[]>(() => {
+    return [...new Set(data?.map(p => p.category))]
+  }, [data]);
 
   return <div className='App'>
-      <ProductList products={testData} categories={categories} selections={selections} addToOrder={addToOrder} />
+      <ProductList products={data ?? []} categories={categories} selections={selections} addToOrder={addToOrder} />
   </div>
 }
 
--- Chapter 19/reactapp/src/data/dataStore.ts
+++ Chapter 19/reactapp/src/data/dataStore.ts
@@ -1,11 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { reducer as selectionsReducer, addToOrder } from "./selectionSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { productsApi, useGetProductsQuery } from "./storeApis";
 
 export const dataStore = configureStore({
     reducer: {
-        "selections": selectionsReducer
-    }
+        "selections": selectionsReducer,
+        [productsApi.reducerPath]: productsApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productsApi.middleware)
 });
 
 export type AppDispatch = typeof dataStore.dispatch;
@@ -16,4 +19,8 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const reducers = {
     addToOrder
+}
+
+export const queries = {
+    useGetProductsQuery
 }
